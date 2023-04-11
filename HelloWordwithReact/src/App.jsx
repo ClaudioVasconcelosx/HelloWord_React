841787c26939147be5c1064fae817a0093d72271
Claudio Vasconcelosx <claudio.vasconcelos@qualitydigital.global>
2023-04-11 16:00:58 -0300

    10 hello world escritos de maneiras diferentes

--- HelloWordwithReact/src/App.jsx
+++ HelloWordwithReact/src/App.jsx
@@ -1,6 +1,22 @@
 function App() {
-	return <h1>Hello Word</h1>;
+	const hello = "Hello World";
+
+	let phrase;
+	return (
+		<>
+			<h1>{phrase = hello.split(' ')}</h1>
+			<h1>{(phrase = hello.split("").join("==="))}</h1>
+			<h1>{(phrase = hello.split("").join("-"))}</h1>
+			<h1>{(phrase = hello.split("").join("/"))}</h1>
+			<h1>{(phrase = hello.split("").join("b"))}</h1>
+			<h1>{(phrase = hello.split("").reverse().join('x'))}</h1>
+			<h1>{(phrase = hello.split("").reverse().join(','))}</h1>
+			<h1>{(phrase = hello.split("").reverse().join('='))}</h1>
+			<h1>{(phrase = hello.split("").reverse().join('@'))}</h1>
+			<h1>{(phrase = hello.split("").reverse().join('bliz'))}</h1>
+			
+		</>
+	);
 }
 
 export default App;
-r
