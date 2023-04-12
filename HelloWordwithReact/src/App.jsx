534bf1f9dff9785adb03339473e92a6a5873b0a7
Claudio Vasconcelosx <claudio.vasconcelos@qualitydigital.global>
2023-04-12 10:44:44 -0300

    refactoring

--- HelloWordwithReact/src/App.jsx
+++ HelloWordwithReact/src/App.jsx
@@ -1,20 +1,18 @@
 function App() {
 	const hello = "Hello World";
 
-	let phrase;
 	return (
 		<>
-			<h1>{phrase = hello.split(' ')}</h1>
-			<h1>{(phrase = hello.split("").join("==="))}</h1>
-			<h1>{(phrase = hello.split("").join("-"))}</h1>
-			<h1>{(phrase = hello.split("").join("/"))}</h1>
-			<h1>{(phrase = hello.split("").join("b"))}</h1>
-			<h1>{(phrase = hello.split("").reverse().join('x'))}</h1>
-			<h1>{(phrase = hello.split("").reverse().join(','))}</h1>
-			<h1>{(phrase = hello.split("").reverse().join('='))}</h1>
-			<h1>{(phrase = hello.split("").reverse().join('@'))}</h1>
-			<h1>{(phrase = hello.split("").reverse().join('bliz'))}</h1>
-			
+			<h1>{hello.split(" ").reverse()}</h1>
+			<h1>{hello.split("").join("===")}</h1>
+			<h1>{hello.split("").join("-")}</h1>
+			<h1>{hello.split("").join("/")}</h1>
+			<h1>{hello.split("").join("b")}</h1>
+			<h1>{hello.split("").reverse().join("x")}</h1>
+			<h1>{hello.split("").reverse().join(",")}</h1>
+			<h1>{hello.split("").reverse().join("=")}</h1>
+			<h1>{hello.split("").reverse().join("@")}</h1>
+			<h1>{hello.split("").reverse().join("bliz")}</h1>
 		</>
 	);
 }
